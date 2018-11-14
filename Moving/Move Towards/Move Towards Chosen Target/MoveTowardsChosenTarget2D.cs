bd3ea064862c66cdb5b73eb416aa731252991d46
stanislaw-zakrzewski <stanislaw.zakrzewski13@gmail.com>
2018-11-14 17:53:45 +0100

    Added moving towards mouse cursor, removed other

--- Moving/Move Towards/Move Towards Chosen Target/MoveTowardsChosenTarget2D.cs
+++ Moving/Move Towards/Move Towards Chosen Target/MoveTowardsChosenTarget2D.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveTowards2D : MonoBehaviour {
+public class MoveTowardsChosenTarget2D : MonoBehaviour {
     private Vector2 target;
     private float maxDistanceDelta;
 
---
+++ Moving/Move Towards/Move Towards Mouse Cursor/MoveTowardsMouseCursor2D.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowardsMouseCursor2D : MonoBehaviour
+{
+    //Projectile is type that is later thrown from center
+    public GameObject projectile;
+    private Stack<GameObject> projectiles;
+    private Vector2 spawnSource;
+    private float power;
+
+    void Start()
+    {
+        //Remember to set all the values required!
+        spawnSource = new Vector2(0, 0);
+        power = 1000;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //In this example move sequence is triggered by mouse 0 click
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject projectileInstance = Instantiate(projectile, spawnSource, Quaternion.identity) as GameObject;
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            float projectileX = mousePosition.x - spawnSource.x;
+            float projectileY = mousePosition.y - spawnSource.y;
+            float r = Mathf.Sqrt(projectileX * projectileX + projectileY * projectileY);
+            Vector2 projectileThrow = new Vector2(projectileX / r, projectileY / r);
+            projectileInstance.GetComponent<Rigidbody2D>().AddForce(projectileThrow * power);
+            projectiles.Push(projectileInstance);
+        }
+    }
+}
