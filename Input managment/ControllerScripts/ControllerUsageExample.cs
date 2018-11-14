15302d4e85b1e61febf20faeaa7e8a28f04e0890
stanislaw-zakrzewski <stanislaw.zakrzewski13@gmail.com>
2018-11-14 09:07:12 +0100

    Added controller scripts, and input management folder

---
+++ Input managment/ControllerScripts/ControllerUsageExample.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ControllerUsageExample : MonoBehaviour {
+    
+    //Default input device is set to Xbox controller, it can be changed in inspector
+    public ControllerEnum inputDevice = ControllerEnum.XBOX_CONTROLLER;
+    private IController controller;
+
+	void Start () {
+        //Adds chosen type of controller to gameobject
+		switch(inputDevice)
+        {
+            case ControllerEnum.XBOX_CONTROLLER:
+                controller = gameObject.AddComponent(typeof(ControllerXbox)) as ControllerXbox;
+                break;
+            case ControllerEnum.MOUSE_KEYBOARD_CONTROLLER:
+                controller = gameObject.AddComponent(typeof(ControllerMouseAndKeyboard)) as ControllerMouseAndKeyboard;
+                break;
+        }
+	}
+}
